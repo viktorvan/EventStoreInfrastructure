db6f0900a54bb761d82a622c3c207be707b13cd8
Viktor Andersson <viktora@activesolution.se>
2017-01-08 23:58:02 +0100

    Apparently we need to check that the projection exits before reading it.

--- EventStoreInfrastructure/EventStoreProjectionService.cs
+++ EventStoreInfrastructure/EventStoreProjectionService.cs
@@ -2,6 +2,7 @@
 using EventStore.ClientAPI.Projections;
 using EventStore.ClientAPI.SystemData;
 using EventStoreInfrastructure.Interfaces;
+using System.Linq;
 
 namespace EventStoreInfrastructure
 {
@@ -18,19 +19,31 @@ namespace EventStoreInfrastructure
 
         public async Task<bool> CreateOrUpdateProjectionAsync(string name, string query)
         {
+            if (!await Exists(name)) return false;
+
             var oldQuery = await _projectionsManager.GetQueryAsync(name, _credentials);
-            if (oldQuery != query)
-            {
-                await _projectionsManager.DeleteAsync(name, _credentials);
-                await _projectionsManager.CreateContinuousAsync(name, query, _credentials);
-                return true;
-            }
-            return false;
+            if (oldQuery == query) return false;
+
+            await _projectionsManager.DeleteAsync(name, _credentials);
+            await _projectionsManager.CreateContinuousAsync(name, query, _credentials);
+
+            return true;
         }
 
         public async Task<string> ReadProjectionResultAsync(string name)
         {
+            if (!await Exists(name))
+            {
+                return null;
+            }
+
             return await _projectionsManager.GetResultAsync(name, _credentials);
+        }
+
+        private async Task<bool> Exists(string name)
+        {
+            var all = await _projectionsManager.ListAllAsync(_credentials);
+            return all.Any(x => x.Name == name);
         }
     }
 }
